6863e3b07af930e409e31766582fb3d2899b2ebc
enrico donelli <e.donelli@incloudteam.com>
2019-08-14 16:36:12 +0200

    creazione finestra

--- rawtool.go
+++ rawtool.go
@@ -13,8 +13,9 @@ import (
 	"time"
 
 	"github.com/enricod/golibraw"
-	"github.com/gotk3/gotk3/gtk"
+	"github.com/gotk3/gotk3/gtk" //"github.com/gotk3/gotk3/gtk"
 	"github.com/nfnt/resize"
+	"github.com/therecipe/qt/widgets"
 )
 
 func extensions() []string {
@@ -62,159 +63,48 @@ func main() {
 	appSettings = Settings{ImagesDir: *imagesdir, WorkDir: dir, OutDir: outdir}
 
 	go readImagesInDir(appSettings.WorkDir)
-	gtk.Init(nil)
 
-	// Create a new toplevel window, set its title, and connect it to the
-	// "destroy" signal to exit the GTK main loop when it is destroyed.
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	win.SetTitle("Simple Example")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
+	// needs to be called once before you can start using the QWidgets
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+
+	// create a window
+	// with a minimum size of 250*200
+	// and sets the title to "Hello Widgets Example"
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetMinimumSize2(250, 200)
+	window.SetWindowTitle("Hello Widgets Example")
+
+	// create a regular widget
+	// give it a QVBoxLayout
+	// and make it the central widget of the window
+	widget := widgets.NewQWidget(nil, 0)
+	widget.SetLayout(widgets.NewQVBoxLayout())
+	window.SetCentralWidget(widget)
+
+	// create a line edit
+	// with a custom placeholder text
+	// and add it to the central widgets layout
+	input := widgets.NewQLineEdit(nil)
+	input.SetPlaceholderText("Write something ...")
+	widget.Layout().AddWidget(input)
+
+	// create a button
+	// connect the clicked signal
+	// and add it to the central widgets layout
+	button := widgets.NewQPushButton2("and click me!", nil)
+	button.ConnectClicked(func(bool) {
+		widgets.QMessageBox_Information(nil, "OK", input.Text(), widgets.QMessageBox__Ok, widgets.QMessageBox__Ok)
 	})
+	widget.Layout().AddWidget(button)
 
-	// Create a new label widget to show in the window.
+	// make the window visible
+	window.Show()
 
-	// Add the label to the window.
-	win.Add(mainPanel())
+	// start the main Qt event loop
+	// and block until app.Exit() is called
+	// or the window is closed by the user
+	app.Exec()
 
-	// Set the default window size.
-	win.SetDefaultSize(800, 600)
-
-	// Recursively show all widgets contained in this window.
-	win.ShowAll()
-
-	// Begin executing the GTK main loop.  This blocks until
-	// gtk.MainQuit() is run.
-	gtk.Main()
-
-	/*
-		fileInfo, err := os.Stat("./_7070001.ORF")
-		if os.IsNotExist(err) {
-			fmt.Printf("errore caricamento %s %s", fileInfo.Name(), err.Error())
-		} else {
-			fmt.Printf("caricamento %s\n", fileInfo.Name())
-			golibraw.Raw2Image(".", fileInfo)
-
-		}
-	*/
-
-}
-
-func mainPanel() *gtk.Widget {
-
-	horBox, err := gtk.BoxNew(gtk.ORIENTATION_HORIZONTAL, 6)
-	if err != nil {
-		log.Fatal("Unable to create horBox:", err)
-	}
-	horBox.SetHomogeneous(false)
-
-	grid, err := gtk.GridNew()
-	if err != nil {
-		log.Fatal("Unable to create grid:", err)
-	}
-	grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
-	grid.SetRowSpacing(6)
-	grid.SetMarginStart(6)
-	grid.SetMarginTop(6)
-
-	entry, err := gtk.EntryNew()
-	if err != nil {
-		log.Fatal("Unable to create entry:", err)
-	}
-	s, _ := entry.GetText()
-	label, err := gtk.LabelNew(s)
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
-	grid.Add(entry)
-	entry.SetHExpand(true)
-	grid.AttachNextTo(label, entry, gtk.POS_RIGHT, 1, 1)
-	label.SetHExpand(true)
-
-	// Connects this entry's "activate" signal (which is emitted whenever
-	// Enter is pressed when the Entry is activated) to an anonymous
-	// function that gets the current text of the entry and sets the text of
-	// the label beside it with it.  Unlike with native GTK callbacks,
-	// (*glib.Object).Connect() supports closures.  In this example, this is
-	// demonstrated by using the label variable.  Without closures, a
-	// pointer to the label would need to be passed in as user data
-	// (demonstrated in the next example).
-	entry.Connect("activate", func() {
-		s, _ := entry.GetText()
-		label.SetText(s)
-	})
-
-	sb, err := gtk.SpinButtonNewWithRange(0, 1, 0.1)
-	if err != nil {
-		log.Fatal("Unable to create spin button:", err)
-	}
-	/*
-		pb, err := gtk.ProgressBarNew()
-		if err != nil {
-			log.Fatal("Unable to create progress bar:", err)
-		}
-	*/
-	grid.Add(sb)
-	sb.SetHExpand(true)
-	//grid.AttachNextTo(pb, sb, gtk.POS_RIGHT, 1, 1)
-	label.SetHExpand(true)
-
-	// Pass in a ProgressBar and the target SpinButton as user data rather
-	// than using the sb and pb variables scoped to the anonymous func.
-	// This can be useful when passing in a closure that has already been
-	// generated, but when you still wish to connect the callback with some
-	// variables only visible in this scope.
-	/*
-		sb.Connect("value-changed", func(sb *gtk.SpinButton, pb *gtk.ProgressBar) {
-			pb.SetFraction(sb.GetValue() / 1)
-		}, pb)
-	*/
-	label, err = gtk.LabelNew("")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
-	}
-	s = "Hyperlink to <a href=\"https://www.cyphertite.com/\">Cyphertite</a> for your clicking pleasure"
-	label.SetMarkup(s)
-	grid.AttachNextTo(label, sb, gtk.POS_BOTTOM, 2, 1)
-
-	dirChooserBtn, err := gtk.FileChooserButtonNew("Dir selection", gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
-	if err != nil {
-		log.Fatal("Unable to create FileChooserDialogNewWith1Button:", err)
-
-	}
-	dirChooserBtn.Connect("selection-changed", dirSelectionChanged)
-	// grid.Add(dirChooserBtn)
-	grid.AttachNextTo(dirChooserBtn, label, gtk.POS_BOTTOM, 3, 1)
-
-	// Some GTK callback functions require arguments, such as the
-	// 'gchar *uri' argument of GtkLabel's "activate-link" signal.
-	// These can be used by using the equivalent go type (in this case,
-	// a string) as a closure argument.
-	//label.Connect("activate-link", func(_ *gtk.Label, uri string) {
-	//	fmt.Println("you clicked a link to:", uri)
-	//})
-
-	horBox.PackStart(grid, false, true, 6)
-
-	flowbox, err = gtk.FlowBoxNew()
-	if err != nil {
-		log.Fatal("Unable to create FileChooserDialogNewWith1Button:", err)
-
-	}
-
-	// popolaFlowbox(flowbox)
-	horBox.PackStart(flowbox, true, true, 6)
-	return &horBox.Container.Widget
-	//return &grid.Container.Widget
-}
-
-func dirSelectionChanged(widget *gtk.FileChooserButton) {
-	fmt.Printf("dir selected %s\n", widget.GetFilename())
-	//appSettings.ImagesDir = widget.GetFilename()
-	//DoExtract(widget.GetFilename())
 }
 
 // IsStringInSlice true if the slice contains the string a
